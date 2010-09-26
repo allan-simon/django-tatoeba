bcc097914779d827345ba14ee3e415b81e067534
SIMON Allan <allan.simon@supinfo.com>
2010-09-26 23:59:07 +0200

    continue PEP-8 code standardisation

--- www_tatoeba/views/index.py
+++ www_tatoeba/views/index.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
+
 def index(request):
-	t = loader.get_template('pages/index.html')
-	c = RequestContext(request, {})
-	return HttpResponse(t.render(c))
+    t = loader.get_template('pages/index.html')
+    c = RequestContext(request, {})
+    return HttpResponse(t.render(c))
+
 
 def home(request):
-	t = loader.get_template('pages/home.html')
-	c = RequestContext(request, {})
-	return HttpResponse(t.render(c))
-
+    t = loader.get_template('pages/home.html')
+    c = RequestContext(request, {})
+    return HttpResponse(t.render(c))
