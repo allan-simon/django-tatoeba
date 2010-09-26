75a06c11d6404a4c2ce3780bcf4176b59f5a0229
SIMON Allan <allan.simon@supinfo.com>
2010-09-26 22:35:03 +0200

    continue PEP-8 code standardisation

--- www_tatoeba/views/sentences.py
+++ www_tatoeba/views/sentences.py
@@ -1,15 +1,17 @@
 from django.template import loader, RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
 
+
 def show(request, sentence_id):
-	t = loader.get_template('pages/sentences.html')
-	c = RequestContext(request, {
-		'sentence_id': sentence_id
-    })
-	return HttpResponse(t.render(c))
+    t = loader.get_template('pages/sentences.html')
+    c = RequestContext(
+        request,
+        {'sentence_id': sentence_id}
+    )
+    return HttpResponse(t.render(c))
+
 
 def random(request):
-	import random
-	r = random.randint(1, 20)
-	return HttpResponseRedirect('/en/sentences/show/%i'%r)
-
+    import random
+    r = random.randint(1, 20)
+    return HttpResponseRedirect('/en/sentences/show/%i' % r)
